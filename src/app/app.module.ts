b118a3168385110539f91208a4fe7ffa24d25c9a
ediz.genc <ediz.genc@yahoo.com>
2023-08-29 13:33:17 +0200

    dev: components directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgramComponent } from './program/program.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { ExcerciseComponent } from './excercise/excercise.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { GuardsComponent } from './guards/guards.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { AdminComponent } from './views/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgramComponent,
+    WorkoutComponent,
+    ExcerciseComponent,
+    NavbarComponent,
+    FooterComponent,
+    GuardsComponent,
+    ProfileComponent,
+    LoginComponent,
+    DashboardComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
