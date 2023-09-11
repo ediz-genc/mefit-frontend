f946a67274cfb105258e1355e316f1ea8b5f2d7f
“linusJtareby” <linus.tareby@se.experis.com>
2023-09-11 11:21:22 +0200

    create-goal-component

--- src/app/components/dashboard/dashboard-create-goal/dashboard-create-goal.component.ts
+++ src/app/components/dashboard/dashboard-create-goal/dashboard-create-goal.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from 'src/app/models/goal.model';
+import { ExerciseService } from 'src/app/services/exercise.service';
+import { GoalService } from 'src/app/services/goal.service';
+import { UserService } from 'src/app/services/user.service';
+import { WorkoutService } from 'src/app/services/workout.service';
 
 @Component({
   selector: 'app-dashboard-create-goal',
@@ -7,7 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardCreateGoalComponent{
 
+  goal: Goal = {
+    goalId: 0,
+    name: '',
+    startDate: '',
+    endDate: '',
+    completed: false,
+    userId: 0,
+    programs: [],
+    completedProgramId: [],
+    workoutId: [],
+    completedWorkoutId: [],
+  }
+
+  constructor(private readonly goalService: GoalService, 
+    private readonly workoutService: WorkoutService, 
+    private readonly userService: UserService, 
+    private readonly exerciseService: ExerciseService ){}
+
   ngOnInit(): void {
       
+    
   }
 }
